46ce91bad828a924c8fb00c8d63f01929f9fa346
bhazalb <bayramhazal030@gmail.com>
2023-05-29 14:35:49 +0300

    anket projesi oluşturuluyor

---
+++ portfolyom/anket.js
@@ -0,0 +1,85 @@
+
+function btnList(){
+  
+}
+
+
+
+
+
+
+
+/*
+document.getElementById('anketForm').addEventListener('submit', function(e) {
+  e.preventDefault();  //yönlendirmeyi kapatt
+
+ const soru1Cevap = document.querySelector('input[name="soru1"]:checked');
+ const soru2Cevap = document.querySelector('input[name="soru2"]:checked');
+ const soru3Cevap = document.querySelector('input[name="soru3"]:checked');
+
+
+ 
+ const  soru1CevapDegeri = soru1Cevap.value;
+ const soru2CevapDegeri = soru2Cevap.value;
+ const soru3CevapDegeri = soru3Cevap.value;
+
+    const sonuclarListesi = document.createElement('li');
+    
+
+     // var sonuclarListesi = document.createElement('ul');
+     const soru1Sonucu = document.querySelector("#list").appendChild(li);
+      soru1Sonucu.textContent = 'Soru 1 Cevabınız: ' + soru1CevapDegeri;
+      sonuclarListesi.appendChild(soru1Sonucu);
+
+      const soru2Sonucu = document.querySelector("#list").appendChild(li);
+      soru2Sonucu.textContent = 'Soru 2 Cevabınız: ' + soru2CevapDegeri;
+      sonuclarListesi.appendChild(soru2Sonucu);
+
+      const soru3Sonucu = document.querySelector("#list").appendChild(li);
+      soru2Sonucu.textContent = 'Soru 3 Cevabınız: ' + soru3CevapDegeri;
+      sonuclarListesi.appendChild(soru3Sonucu);
+
+      const sonuclarDiv = document.getElementById('sonuclar');
+      sonuclarDiv.innerHTML = '';
+      sonuclarDiv.appendChild(sonuclarListesi);
+
+    /*  document.getElementById('sonuclar').innerHTML = '<h2>Soru 1 Cevabınız: ' + soru1CevapDegeri + '</h2>' +
+                                                      '<h2>Soru 2 Cevabınız: ' + soru2CevapDegeri + '</h2>' +
+                                                      '<h2>Soru 3 Cevabınız: ' + soru3CevapDegeri + '</h2>';
+
+      // Formu sıfırla
+     document.getElementById('anketForm').reset();
+
+      alert('Lütfen tüm soruları cevaplayın.');}
+*/
+      /*     
+const soru1Cevap = document.querySelector('input[name="soru1"]:checked');
+const soru2Cevap = document.querySelector('input[name="soru2"]:checked');
+const soru3Cevap = document.querySelector('input[name="soru3"]:checked');
+
+
+               function newElement (){
+
+                const  soru1CevapDegeri = soru1Cevap.value;
+                const soru2CevapDegeri = soru2Cevap.value;
+                const soru3CevapDegeri = soru3Cevap.value;
+
+                console.log(soru1CevapDegeri);
+                console.log(soru2CevapDegeri);
+                console.log(soru3CevapDegeri);
+
+
+
+              }*/
+/*
+              const radioButtons = document.getElementsByName("A");
+              const selectedValueElement = document.getElementById("selectedValue");
+            
+              // Add event listener to each radio button
+              radioButtons.forEach(function (radio) {
+                radio.addEventListener("change", function () {
+                  if (this.checked) {
+                    selectedValueElement.textContent = "Selected Value: " + this.value;
+                  }
+                });
+              });*/
