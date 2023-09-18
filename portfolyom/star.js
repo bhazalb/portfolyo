ecb7f54e918d15715ec359cc7101599caa1faf82
bhazalb <bayramhazal030@gmail.com>
2023-09-18 13:02:04 +0300

    commit

--- portfolyom/star.js
+++ portfolyom/star.js
@@ -1,1 +1,16 @@
-
+/*document.addEventListener('DOMContentLoaded', function() {
+    var dropdownToggle = document.querySelector('.dropdown-toggle');
+    var dropdownMenu = document.querySelector('.dropdown-menu');
+  
+    dropdownToggle.addEventListener('click', function() {
+      dropdownMenu.classList.toggle('show');
+    });
+  
+    document.addEventListener('click', function(event) {
+      var target = event.target;
+      if (!dropdownToggle.contains(target) && !dropdownMenu.contains(target)) {
+        dropdownMenu.classList.remove('show');
+      }
+    });
+  });
+  */
